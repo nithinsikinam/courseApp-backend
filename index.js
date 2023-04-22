7e82feb5f97a8c97dbebba2d38c14f1198d62ee9
nithinsikinam <nithinsikinam@gmail.com>
2023-04-23 00:04:20 +0530

    added alot of stuff

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express = require('express');
 const db = require('./db');
 const app = express();
 
-app.get("/", function(req, res){
-    res.send("hello")
-})
+app.use("/userProfile",)
 
 app.listen(3000,() =>{console.log("Server Started")})
---
+++ routers/userProfile.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const router = express.Router();
+
+router.post('/login', async (req,res) => {
+    console.log(req.body._id);
+     console.log(req.body.Password);
+    const search = await users.find({"_id":req.body._id})
+    if(search.length==1){      
+    if(await bcrypt.compare(req.body.Password,search[0]["Password"])){
+    const id = req.body._id;
+    const user = {"id":id} ;
+    const accessToken = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET)   
+    res.json({"value":"yes","access_token":accessToken})
+    }else{
+    res.json({"value":"no"})
+   }}
+    else{
+    res.json({"value":"no"})
+    }
+})
+
+router.post('/register', async (req,res) => { 
+    const search = await users.find({"_id":req.body._id})
+    if(search.length!==0){
+        
+        res.json({"value":"no"})
+    }
+else{
+    const hashedPassword = await bcrypt.hash(req.body.Password,10)
+    const user = await users.create({
+        Username:req.body.Username,
+        Password:hashedPassword,
+        _id: req.body._id,
+        Semester:req.body.Semester,
+        Group:req.body.Group
+})
+await user.save()
+res.json({"value":"yes"})
+    }
+    })
+
+function authenticateToken(req,res,next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if(token == null) return res.sendStatus(401)
+    
+    jwt.verify(token,process.env.ACCESS_TOKEN_SECRET,(err,user) =>{
+        if (err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}  
