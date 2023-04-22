7326e6cf8ebc6aa334e561295e55a15e1b4c0843
Shashank More <shashankmore210@gmail.com>
2023-04-23 00:55:22 +0530

    changes

--- schemas/users.js
+++ schemas/users.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
 
 const usersSchema = new mongoose.Schema({
-  UserId:String,
-  EmailId:String,
-  Password:String,
-  Intrests:String,
-  ProfilePic:String,
+  userid:String,
+  emailid:String,
+  password:String,
+  intersets:String,
+  profilepic:String,
   verified:Boolean
 })
 
